f18d869f89f18f868dc5c6066d90cc8e107af813
shexiaokai <shexk123@163.com>
2018-02-10 21:08:08 +0800

    201-02-10
    1.基本功能全部实现
    2.文档注释完成
    3.呼吸灯过亮（小问题可忽略）

--- Program/ASM/App/Key.asm
+++ Program/ASM/App/Key.asm
@@ -1,25 +1,25 @@
 /**
-  **********************************  CC2530  **********************************
-  * @文件名     ： Key.asm;
-  * @作者       ： 佘晓凯;
-  * @文件版本   ： V1.0.0;
-  * @日期       ： 2018年02月05日;
-  * @摘要       ： 按键部分;
+  **********************************  STC89C52  ********************************
+  * @文件名     ： Key.asm 
+  * @作者       ： 佘晓凯 
+  * @文件版本   ： V1.0.0 
+  * @日期       ： 2018年02月10日 
+  * @摘要       ： 按键部分 
   ******************************************************************************/
 /*----------------------------------------------------------------------------
   更新日志:
-  2018-01-25 V1.0.0:
+  2018-02-10 V1.0.0:完成基本部分 
   ----------------------------------------------------------------------------*/
 
 /* 包含的头文件 --------------------------------------------------------------*/
 $INCLUDE (MAIN.inc)
 $INCLUDE (Delay.inc)
-
+$INCLUDE (Time.inc)
 
 
 KEY_ASM 	SEGMENT		CODE
 RSEG		KEY_ASM
-
+;函数 
 PUBLIC KEY
 
 /************************************************
@@ -32,7 +32,6 @@ PUBLIC KEY
 			使用累加器A，未堆栈；			
 	
 *************************************************/
-
 KEY:
 		MOV		A	,	P1			
 		ANL		A	,	#07H		;取后三位；
@@ -44,35 +43,58 @@ KEY:
 		ANL		A	,	#07H
 		XRL		A	,	#07H 
 		JZ		KEY_RETURN			;如果没有按下，return；
-									;如果按下；
-		MOV		P1	,	#00H
-			
+		;如果按下；
+		MOV		P2	,	#00H		;按键按下的时候把灯熄灭 
+		CLR		TR0					;把定时器关闭 
+		;判断按下的按键 	
 		CJNE	A	,	MODE	,CHANGE_MODE	;如果按键和当前模式不一样，跳转	
-		;如果不改变模式；
+		;如果不改变模式 
+SAMEMODE1:	
+		CJNE	A	,	#01H	,	SAMEMODE2	;如果为模式1改变闪烁灯位置
+		MOV		A	,	R3						;R3为闪烁灯位置 
+		RL		A								;闪烁灯位置左移 
+		MOV		R3	,	A
+		MOV		COUNT,	#250					;计数值重置 
+		SJMP	KEYUP							;ruturn
+SAMEMODE2:	
+		CJNE	A	,	#02H	,	SAMEMODE3	;如果为模式2
+		CPL		MODE2LOR						;改变流水灯方向 
+		MOV		P2	,	R2						;R2纪录流水灯状态 
+		MOV		COUNT,	#250					;重置计数值 
+		SJMP	KEYUP							;return
 		
-		
+SAMEMODE3:
+		SETB	TR0								;开启定时器 
+		CJNE	R7	,	#10H	,	INCR5		;呼吸频率上限16 
+		MOV		R7	,	#03H					;下限3 
+INCR5:	INC		R7								;改变呼吸频率 
+		SJMP	KEYUP							;return
+
+;如果模式改变 
 CHANGE_MODE:
-		MOV		MODE,	A
-		
-		
+		MOV		MODE,	A						;模式变量改变  
+				
 MODE1:	
-		CJNE	A	,	#01H	,	MODE2	;如果为模式1
-		CLR		RS0					;设置第0工作区;
-		CLR		RS1
+		CLR 	TR0
+		CJNE	A	,	#01H	,	MODE2	;如果改为模式1
+		MOV		P2	,	R3					
+		MOV		COUNT,	#250
 		SJMP	KEYUP
 
 MODE2:	
-		CJNE	A	,	#02H	,	MODE3	;如果为模式2
-		CLR		RS0					;设置第1工作区;
-		SETB	RS1
+		CLR 	TR0
+		CJNE	A	,	#02H	,	MODE3	;如果改为模式2
+		MOV		P2	,	R2
+		MOV		COUNT,	#250
 		SJMP	KEYUP
-		
+											;如果改为模式3
 MODE3:	
-		SETB	RS0					;设置第2工作区;
-		CLR		RS1
+		SETB 	TR0							;开启定时器 
+		MOV		R4	,	#20					;初始化数据 
+		MOV		R5	,	#0					
+		SETB 	TR0
 		SJMP	KEYUP
 		
-
 KEYUP:
 		MOV		A	,	P1			
 		ANL		A	,	#07H		;取后三位；
